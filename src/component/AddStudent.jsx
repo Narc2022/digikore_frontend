d10aede1a36e3c9a3cc6913e834086de325752a8
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-03-13 00:07:07 +0530

    create student

--- src/component/AddStudent.jsx
+++ src/component/AddStudent.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DownloadCsv from "./DownloadCsv";
+import axios from "axios";
 
 function AddStudent() {
   const [formData, setFormData] = useState({
@@ -17,6 +18,15 @@ function AddStudent() {
     language_proficiency: "",
     questions_concerns: "",
   });
+  const [eotp, setEotp] = useState(false);
+  const [eotpValue, setEotpValue] = useState("");
+  const [eotpVerified, setEotpVerified] = useState("");
+
+  const [otp, setOtp] = useState(false);
+  const [otpValue, setOtpValue] = useState("");
+  const [otpVerified, setOtpVerified] = useState("");
+
+  const [error, setError] = useState();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -25,12 +35,175 @@ function AddStudent() {
       [name]: value,
     });
   };
-
+  const handleEotpChange = (e) => {
+    setEotpValue(e.target.value);
+    console.log("e.target.value", e.target.value);
+  };
+  const handleOtpChange = (e) => {
+    setOtpValue(e.target.value);
+    console.log("e.target.value", e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
   };
-
+  const verifyEOTP = () => {
+    axios
+      .post(
+        "https://paytm-sv70.onrender.com/api/v1/student/verify-otp",
+        { email: formData.email, otp: eotpValue },
+        {
+          headers: {
+            "Content-Type": "application/json", // Specify content type as JSON
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Success:", response.data); // Log the response data
+        setEotpVerified(response.data.success);
+      })
+      .catch((error) => {
+        console.error("Error:", error); // Log any errors
+      });
+  };
+  const verifyOTP = () => {
+    axios
+      .post(
+        "https://paytm-sv70.onrender.com/api/v1/student/verify/mobile",
+        { mobileNumber: formData.contact, otp: otpValue },
+        {
+          headers: {
+            "Content-Type": "application/json", // Specify content type as JSON
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Success:", response.data); // Log the response data
+        setOtpVerified(response.data.success);
+      })
+      .catch((error) => {
+        console.error("Error:", error); // Log any errors
+      });
+  };
+  const sendEOTP = () => {
+    if (!!formData.email) {
+      axios
+        .post(
+          "https://paytm-sv70.onrender.com/api/v1/student/send-otp",
+          { email: formData.email },
+          {
+            headers: {
+              "Content-Type": "application/json", // Specify content type as JSON
+            },
+          }
+        )
+        .then((response) => {
+          console.log("Success:", response.data); // Log the response data
+          if (response.data.message === "OTP sent successfully") {
+            setEotp(true);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error); // Log any errors
+        });
+    }
+  };
+  const sendOtp = () => {
+    axios
+      .post(
+        "https://paytm-sv70.onrender.com/api/v1/student/sendotp",
+        { mobileNumber: formData.contact },
+        {
+          headers: {
+            "Content-Type": "application/json", // Specify content type as JSON
+          },
+        }
+      )
+      .then((response) => {
+        console.log("Success:", response.data); // Log the response data
+        if (response.data.message === "OTP sent successfully") {
+          setOtp(response.data.success);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error); // Log any errors
+      });
+  };
+  const onFormSubmit = () => {
+    if (
+      !formData.full_name ||
+      !formData.email ||
+      !formData.contact ||
+      !formData.age ||
+      !formData.gender ||
+      !formData.address ||
+      !formData.academic_level ||
+      !formData.current_school ||
+      !formData.field_of_study ||
+      !formData.study_destination ||
+      !formData.enrollment_year ||
+      !formData.language_proficiency ||
+      !formData.questions_concerns
+    ) {
+      setError("please fill the missing fields");
+    } else {
+      if (!eotpVerified || !otpVerified) {
+        setError("please verify phone/email");
+      } else {
+        axios
+          .post(
+            "https://paytm-sv70.onrender.com/api/v1//student/create",
+            {
+              fullName: formData.full_name,
+              email: formData.email,
+              contact: formData.contact,
+              age: formData.age,
+              gender: formData.gender,
+              address: formData.address,
+              currentAcademicLevel: formData.academic_level,
+              currentSchool: formData.current_school,
+              fieldOfStudy: formData.field_of_study,
+              desireStudyDestination: formData.study_destination,
+              expectedYear: formData.enrollment_year,
+              languageProficiency: formData.language_proficiency,
+              anySpecificQuestion: formData.questions_concerns,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json", // Specify content type as JSON
+              },
+            }
+          )
+          .then((response) => {
+            setFormData({
+              full_name: "",
+              email: "",
+              contact: "",
+              age: "",
+              gender: "male",
+              address: "",
+              academic_level: "high_school",
+              current_school: "",
+              field_of_study: "",
+              study_destination: "",
+              enrollment_year: "",
+              language_proficiency: "",
+              questions_concerns: "",
+            });
+            setEotp(false);
+            setEotpValue("");
+            setEotpVerified("");
+            setOtp(false);
+            setOtpValue("");
+            setOtpVerified("");
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error("Error:", error); // Log any errors
+          });
+      }
+    }
+  };
   return (
     <div className="registration-form">
       <h2>Registration Form</h2>
@@ -46,7 +219,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="email">Email Address:</label>
         <br />
         <input
@@ -55,10 +227,25 @@ function AddStudent() {
           name="email"
           value={formData.email}
           onChange={handleChange}
-        />
+        />{" "}
+        {eotpVerified ? (
+          "verified"
+        ) : eotp ? (
+          <>
+            <input
+              type="eotp"
+              id="eotp"
+              name="eotp"
+              value={eotpValue}
+              onChange={handleEotpChange}
+            />
+            <button onClick={verifyEOTP}>verify</button>
+          </>
+        ) : (
+          <button onClick={sendEOTP}>send otp</button>
+        )}
         <br />
         <br />
-
         <label htmlFor="contact">Contact:</label>
         <br />
         <input
@@ -68,9 +255,24 @@ function AddStudent() {
           value={formData.contact}
           onChange={handleChange}
         />
+        {otpVerified ? (
+          "verified"
+        ) : otp ? (
+          <>
+            <input
+              type="eotp"
+              id="eotp"
+              name="eotp"
+              value={otpValue}
+              onChange={handleOtpChange}
+            />
+            <button onClick={verifyOTP}>verify</button>
+          </>
+        ) : (
+          <button onClick={sendOtp}>send otp</button>
+        )}
         <br />
         <br />
-
         <label htmlFor="age">Age:</label>
         <br />
         <input
@@ -82,7 +284,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="gender">Gender:</label>
         <br />
         <select
@@ -97,7 +298,6 @@ function AddStudent() {
         </select>
         <br />
         <br />
-
         <label htmlFor="address">Address:</label>
         <br />
         <textarea
@@ -109,7 +309,6 @@ function AddStudent() {
         ></textarea>
         <br />
         <br />
-
         <label htmlFor="academic_level">Current Academic Level:</label>
         <br />
         <select
@@ -124,7 +323,6 @@ function AddStudent() {
         </select>
         <br />
         <br />
-
         <label htmlFor="current_school">
           Name of Current School/University:
         </label>
@@ -138,7 +336,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="field_of_study">Intended Field of Study:</label>
         <br />
         <input
@@ -150,7 +347,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="study_destination">Desired Study Destination:</label>
         <br />
         <input
@@ -162,7 +358,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="enrollment_year">Expected Year of Enrollment:</label>
         <br />
         <input
@@ -174,7 +369,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="language_proficiency">Language Proficiency:</label>
         <br />
         <input
@@ -186,7 +380,6 @@ function AddStudent() {
         />
         <br />
         <br />
-
         <label htmlFor="questions_concerns">
           Any Specific Questions or Concerns:
         </label>
@@ -200,8 +393,15 @@ function AddStudent() {
         ></textarea>
         <br />
         <br />
-
-        <input type="submit" value="Submit" />
+        {error ? error : ""}
+        <button
+          type="submit"
+          value="Submit"
+          className="button"
+          onClick={onFormSubmit}
+        >
+          Submit
+        </button>
       </form>
       <DownloadCsv />
     </div>
